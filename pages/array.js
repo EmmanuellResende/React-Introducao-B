c18f70e4072a4953f2bdea3cc18817fdd6d9153d
Emmanuell Resende de Melo Gomes <emmanuell_resende@hotmail.com>
2023-04-04 21:52:16 -0300

    Criando o projeto

---
+++ pages/array.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Cabecalho from '../components/Cabecalho'
+
+const array = () => {
+    return (
+        <>
+            <Cabecalho />
+            <div className='bg-secondary py-3 text-white text-center mb-3'>
+                <Container>
+                    <h1 className='display-4'>Array</h1>
+                </Container>
+            </div>
+            <h1 className='text-danger text-center'>Hello World!</h1>
+
+            <div style={{width: '100%'}} className='bg-secondary position-fixed bottom-0 py-3 text-white text-center'>
+                <p>Todos os diretos reservados®</p>
+            </div>
+        </>
+    )
+}
+
+export default array
